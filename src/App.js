1ab5dddab1bbcf30444e62c21ac71287c99960a8
LitvinDaria <frldlit@gmail.com>
2021-11-23 15:54:43 +0900

    без явно заданной даты

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import GetTime from './GetTime';
    
    
 function GetDateToday(props) {
-  var today = new Date(2021, 11, 11, 0, 15, 0, 0);
-  const hour = today.getHours();
-  const wish = `${(hour < 6 && 'Ночь') || (hour < 12 && 'Утро')|| (hour < 18 && 'День') || 'Вечер'} `;   
+  var today = new Date();
+  const hour = today.getHours();  
+  const wish = "";
+  //const wish = `${(hour < 6 && 'Ночь') || (hour < 12 && 'Утро')|| (hour < 18 && 'День') || 'Вечер'} `;   
+  /*if (hour >= 0 && hour < 6)  wish = "ночь";  
+  else if (hour >= 6 && hour < 13) wish = "утро";
+  //else if (hour >= 13 && hour < 18) wish = "день";
+  //else if (hour > 12 && hour <= 18) wish = "день";
+  //else if (hour > 18 && hour <= 0){ wish = "вечер";}*/
+  
   return (
     <div>
     <h1>Дата: {today.toLocaleDateString() + ""} </h1>
     <GetTime />
-    <h1>Время суток: {wish} </h1>
+    <h1>Время суток: {(hour < 6 ? 'Ночь': (hour < 12 ? 'Утро': (hour < 18 ? 'День' : 'Вечер') ) )} </h1>
     </div>
    
 )
